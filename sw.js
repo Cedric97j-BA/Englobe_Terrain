ea32f14fb24e619330bc9c91fd12bbee824e19bb
Cedric Bien-Aime <bienaime009@gmail.com>
2026-09-02 10:44:06 -0400

    Minor update 3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Changement du nom pour forcer la mise à jour sur les tablettes
-const CACHE_NAME = 'hub-inspection-v1.0.0.0';
+const CACHE_NAME = 'hub-inspection-v1.0.0.1';
 
 const ASSETS = [
     './',
